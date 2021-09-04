095c740d3ec95219abef51cf4750c96edf13ab48
suyuying <z0952657360@gmail.com>
2021-09-05 00:37:42 +0800

    test master

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message":"HIHI",
+			"message":"This is Master",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
