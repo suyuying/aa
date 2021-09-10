8086d89aafb879286db1c4ed5d35ce84601d4c26
suyuying <z0952657360@gmail.com>
2021-09-10 09:10:06 +0800

    tag 3.0.2

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message":"This is develop , good job",
+			"message":"This is master ",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
